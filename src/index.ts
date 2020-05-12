fefe01ac09c5980a82281621b715e207a790f859
Emmanuel312 <efn@cin.ufpe.br>
2020-05-11 21:40:21 -0300

    test buddy works

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.json({ message: "work" });
+  return res.json({ buddy: "works" });
 });
 
 app.listen(PORT, HOST, () => console.log("Server on por 3000"));
