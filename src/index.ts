7b9eaf01394950238362dcf76c47257e1b9773fc
Emmanuel312 <efn@cin.ufpe.br>
2020-05-11 21:31:57 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const PORT = 3000;
+const HOST = "0.0.0.0";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.json({ message: "work" });
+});
+
+app.listen(PORT, HOST, () => console.log("Server on por 3000"));
