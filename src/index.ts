7de2e35b579a62a82d83d5393524e93202e337fb
Emmanuel312 <efn@cin.ufpe.br>
2020-05-13 21:23:59 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.json({ test: "ci cd" });
+  return res.json({ buddy: "works" });
 });
 
 app.listen(PORT, HOST, () => console.log("Server on por 3000"));
