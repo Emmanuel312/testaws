8f2197d2d5933fe9b31995e65ece790dd3aa7cce
Emmanuel312 <efn@cin.ufpe.br>
2020-05-15 11:58:09 -0300

    add nginx

---
+++ nginx/conf.d
@@ -0,0 +1,15 @@
+server {
+  server_name ec2-3-82-53-176.compute-1.amazonaws.com;
+
+  location / {
+    proxy_pass http://127.0.0.1:3000;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-Proto $scheme;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_cache_bypass $http_upgrade;
+  }
+}
