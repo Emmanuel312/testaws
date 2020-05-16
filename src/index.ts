7824ea8d650df7b0250f9ad05bf31d7c280c373e
Emmanuel312 <efn@cin.ufpe.br>
2020-05-15 21:55:17 -0300

    docker compose

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.json({ buddy: "works again" });
+  return res.json({ buddy: "docker compose" });
 });
 
 app.listen(PORT, HOST, () => console.log("Server on por 3000"));
