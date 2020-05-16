22aded832635da8d5fe163c45eb8de0371574054
Emmanuel312 <efn@cin.ufpe.br>
2020-05-16 18:10:04 -0300

    add database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoomModule } from './room/room.module';
+import { typeOrmConfig } from './config/typeorm.config';
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), RoomModule],
   controllers: [AppController],
   providers: [AppService],
 })
